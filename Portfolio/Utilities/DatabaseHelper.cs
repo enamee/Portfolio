2961c364e99f04ae8acf14f4338978c50e4c51b5
appreel <saphrstorm@gmail.com>
2025-09-06 12:05:15 +0600

    Add DatabaseHelper.cs

---
+++ Portfolio/Utilities/DatabaseHelper.cs
@@ -0,0 +1,60 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Portfolio.Utilities
+{
+    public class DatabaseHelper
+    {
+        private static readonly string cs = ConfigurationManager.ConnectionStrings["PortfolioDBConnection"].ConnectionString;
+
+        public static DataTable ExecuteSelect(string query, params SqlParameter[] parameters)
+        {
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    if (parameters != null)
+                        cmd.Parameters.AddRange(parameters);
+
+                    using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                    {
+                        DataTable dt = new DataTable();
+                        da.Fill(dt);
+                        return dt;
+                    }
+                }
+            }
+        }
+
+        public static int ExecuteNonQuery(string query, params SqlParameter[] parameters)
+        {
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    if (parameters != null)
+                        cmd.Parameters.AddRange(parameters);
+
+                    con.Open();
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static object ExecuteScalar(string query, params SqlParameter[] parameters)
+        {
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    if (parameters != null)
+                        cmd.Parameters.AddRange(parameters);
+
+                    con.Open();
+                    return cmd.ExecuteScalar();
+                }
+            }
+        }
+    }
+}
